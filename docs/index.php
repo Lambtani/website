bccd2b085fdcb78910450b8d76e5f12031a9d410
lambtani <lambtani@gmail.com>
2024-06-18 16:33:14 +0900

    ああ

---
+++ docs/index.php
@@ -0,0 +1,103 @@
+<html lang="ja">
+<head>
+	<meta charset="utf-8">
+	<title>Ryosuke Iritani</title>
+    <link rel="stylesheet" href="css/normalize.css">
+    <link rel="stylesheet" href="css/style.css">
+	<link rel="icon" href="img/favicon.jpg">
+	<link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
+
+
+	<script type="text/javascript" async
+  src="https://cdnjs.cloudflare.com/ajax/libs/mathjax/2.7.1/MathJax.js?config=TeX-AMS_CHTML">
+</script>
+
+<script src="https://kit.fontawesome.com/9a727b337c.js" crossorigin="anonymous"></script>
+
+    <!-- <link rel="icon" href="common/images/icon.ico"> -->
+    </head>
+<body>  
+<div id="page">
+<header id="pageHead">  
+<!-- <img src="common/images/hongo.001.png" alt ="" width="980" height="80"> -->
+	<!-- <h1 id="siteTitle" align="center">Masaru HONGO's Website</h1> -->
+	<h1 id="siteTitle">Ryosuke Iritani&rsquo;s Website 	
+		</h1>
+	<nav class="globalNavi">
+		<ul>
+			<li  class="current">
+				<a href="index.html">
+					<i class="fa-solid fa-house" style="font-size: 40px; color: #4f4d4d""></i>
+					<br>home</br>
+				</a></li>
+			<li>
+				<a href="CV.html">
+					<i class="fa-regular fa-address-card" style="font-size: 40px; color: #4f4d4d""></i>
+					<br>cv/contact</br>
+				</a></li>
+			<li>
+				<a href="publications.html"> 
+				<i class="fa-solid fa-book" style="font-size: 40px; color: #4f4d4d""></i>
+				<br>publications</br>
+				</a></li>
+			<li>
+				<a href="japanese.html">
+				<i class="fa-solid fa-language" style="font-size: 40px; color: #4f4d4d""></i>
+				<br>日本語</br>
+				</a></li>	
+			<li>
+				<a href="Mentorship.html">
+				<span class="material-symbols-outlined" style="font-size: 40px; color: #4f4d4d">
+						diversity_3
+						</span>
+				<!-- <i class="fa-solid fa-user-group" style="font-size: 40px; color: #6f6c6c"></i> -->
+				<br>mentorship</br>
+				</a></li>	
+			<li>
+				<a href="research.html">
+				<i class="fa-solid fa-graduation-cap" style="font-size: 40px; color: #4f4d4d"></i>
+				<br>research</br>
+			</a></li>	
+			<li>
+				<a href="misc.html"> 
+					<span class="material-symbols-outlined" style="font-size: 40px; color: #4f4d4d">
+						psychology
+						</span>
+				<br>philosophy</br>
+			</a></li>
+			<!-- <li><a href="misc.html"> What am I?</a></li> -->
+		</ul>
+	</nav> 
+</header>
+
+
+<!-- <div id="pageBody"> -->
+	<div id="pageBodyMain">
+	<article class="articleDetail">
+		<header class="articleDetailHead">
+			<h2 class="heading-typeB">What&rsquo;s new?</h2>
+            <!-- <div class="main-img"> -->
+                <img class="round-img" src="img/komame01.jpg" align="right" alt="こまめ">
+				<ul class="a">
+					<li>2024/05/13：長らく更新してませんでしたが、学術変革A（分担）および学術変革A（公募）に採択されました。<a href="https://jrecin.jst.go.jp/seek/SeekJorDetail?ln=1&amp;id=D124050092&amp;ln_jor=1">ポスドクも募集</a>しています。</li>
+					<li>2024/04/25：科学技術週間関連行事として、<a href="https://www.jimin.jp/news/information/208037.html">自民党本部</a>にて発表しました。</li>
+					<li> 2023/11/20：I organized a graph-theory workshop (with five invited speakers) and a colloquium talk (inviting Prof Dr. Yukie Sano)!</li>
+					<li> 2023/10/07-13：Visiting Oxford as an external examiner of <a href="https://www.oxfordreference.com/display/10.1093/oi/authority.20110803120125489">viva</a> (Ph.D. thesis). What a great city!
+					<li>2023/07/19：所属機関で研究内容などを紹介してもらいました。くわしくは<a href="https://www.riken.jp/pr/closeup/2023/20230607_1/index.html">こちら</a></li>
+					<li>2023/04/01：上級研究員になりました。今後とも宜しくお願いいたします。</li>
+					<li>2023/03/20：Oikosに出版した論文がEditor&#8217;s Choiceになりました！</li>
+					<li>2023/01/27：本が出ました！<a href="https://www.bun-ichi.co.jp/tabid/57/pdid/978-4-8299-6209-1/Default.aspx">『植物の行動生態学』</a>の第５章で血縁淘汰理論について解説しております。</li>
+					<li>2022/12/16：<a href="https://www.bigissue.jp/backnumber/445/">ビッグイシュー・ジャパン445号</a>に寄稿しました！「私のサードプレイス」について書きました！</li>
+					<li>Spotify『すごい進化ラジオ』で、性比の理論の解説を行っています。<br />
+					性比シーズン<a href="https://open.spotify.com/episode/7yJQknPKVRredi3YUYmjIy">エピソード１：</a>Fisher性比。<br />
+					性比シーズン<a href="https://open.spotify.com/episode/14dtbmOwGMS0yKhyg1dS97">エピソード２：</a>LMC理論。<br />
+					性比シーズン<a href="https://open.spotify.com/episode/2BDbClHw4IA8R8FsfgTkov">エピソード３：</a>Yamaguchi 1985 理論について。<br />
+					性比シーズン<a href="https://open.spotify.com/episode/3u3oyxvHu7j9SsGItT7e4l">エピソード４：</a>Trivers-Willard仮説について。</li>
+					</ul>
+            <!-- </div> -->
+
+			\[
+			RB-C - \rho (B-C)>0		  \]
+</body>
+
+</html>
