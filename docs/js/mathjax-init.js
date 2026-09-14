b60877e0c7b987641c91928e09285972d8fdbc8d
lambtani <lambtani@gmail.com>
2026-09-14 10:58:44 +0900

    updated

--- docs/js/mathjax-init.js
+++ docs/js/mathjax-init.js
@@ -9,6 +9,13 @@
  *   インライン: $E=mc^2$  または  \(E=mc^2\)
  *   ディスプレイ: $$E=mc^2$$  または  \[E=mc^2\]
  *
+ * MathJax v3にはフォント切り替えの仕組みがまだ無いため、v4を使用。
+ * 数式フォントは Pagella（Palatino系、他の欧文フォント候補と相性が良い）。
+ * v4のフォントパッケージは執筆時点でベータ版（4.0.0-beta.x）なので、
+ * 稀に細かい表示崩れが出る可能性がある。気になる場合は下の
+ * output.font を 'mathjax-tex'（従来のMathJax標準フォント）や
+ * 'mathjax-stix2'（Times系）などに変えれば良い。
+ *
  * 設定を変えたいときは、このファイルだけ直せば全ページに反映される。
  */
 window.MathJax = {
@@ -17,7 +24,8 @@ window.MathJax = {
     displayMath: [['$$', '$$'], ['\\[', '\\]']],
     processEscapes: true
   },
-  svg: { fontCache: 'global' }
+  svg: { fontCache: 'global' },
+  output: { font: 'mathjax-pagella' }
 };
 
 (function () {
@@ -27,6 +35,6 @@ window.MathJax = {
   var script = document.createElement('script');
   script.id = 'MathJax-script';
   script.async = true;
-  script.src = 'https://cdnjs.cloudflare.com/ajax/libs/mathjax/3.2.2/es5/tex-svg.js';
+  script.src = 'https://cdn.jsdelivr.net/npm/mathjax@4/tex-svg.js';
   document.head.appendChild(script);
 })();
